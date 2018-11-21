5509af3d94f5492062ec0afdb9a5bf1d0339a839
sfndude <hadoepan@gmail.com>
2018-11-21 17:49:28 +0700

    add new source rekursif_genap_ganjil.cpp

--- rekursif_genap_ganjil.cpp
+++ rekursif_genap_ganjil.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
 
-void genap_ganjil(int _first, int _end){
-    if(_first%2==0)
-        std::cout<<_first<<" adalah GENAP"<<std::endl;
-    else
-        std::cout<<_first<<" adalah GANJIL"<<std::endl;
+void genap_ganjil(int _first, int _end, bool mode){
+    if(mode==1){
+        if(_first<_end){
+            if(_first%2==0)
+                std::cout<<_first<<" adalah GENAP"<<std::endl;
 
-    if(_first==_end)
-        std::cout<<"Selesai!\n";
-    else
-        genap_ganjil(_first+1,_end);
+            if(_first==_end)
+                std::cout<<"Selesai!\n";
+            else
+                genap_ganjil(_first+1,_end,1);
+        }else{
+            if(_first%2==0)
+                std::cout<<_first<<" adalah GENAP"<<std::endl;
 
+            if(_first==_end)
+                std::cout<<"Selesai!\n";
+            else
+                genap_ganjil(_first-1,_end,1);
+        }
+    }else{
+        if(_first<_end){
+            if(_first%2!=0)
+                std::cout<<_first<<" adalah GANJIL"<<std::endl;
+
+            if(_first==_end)
+                std::cout<<"Selesai!\n";
+            else
+                genap_ganjil(_first+1,_end,0);
+        }else{
+            if(_first%2!=0)
+                std::cout<<_first<<" adalah GANJIL"<<std::endl;
+
+            if(_first==_end)
+                std::cout<<"Selesai!\n";
+            else
+                genap_ganjil(_first-1,_end,0);
+        }
+    }
 }
 
 int main(void){
-    genap_ganjil(1,10);
+    int start, stop, mode;
+
+    std::cout<<"Masukkan nilai mulai : ";
+    std::cin>>start;
+    std::cout<<"Masukkan nilai ahir : ";
+    std::cin>>stop;
+    std::cout<<"Masukkan (1=genap/0=ganjil) : ";
+    std::cin>>mode;
+
+    std::cout<<std::endl;
+    genap_ganjil(start,stop,mode);
     return 0;
 }
